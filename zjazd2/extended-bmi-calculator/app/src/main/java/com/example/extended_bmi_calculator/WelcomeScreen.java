8994b64b7b834408506d125ca5b07a4bc1522f15
Adam Łuszcz <adam.luszcz20@gmail.com>
2024-05-10 15:15:51 +0200

    Add task 2

---
+++ zjazd2/extended-bmi-calculator/app/src/main/java/com/example/extended_bmi_calculator/WelcomeScreen.java
@@ -0,0 +1,26 @@
+package com.example.extended_bmi_calculator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+
+
+        Button bmiCalculatorButton = findViewById(R.id.startButton);
+        bmiCalculatorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeScreen.this, Menu.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
